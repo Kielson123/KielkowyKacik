631302ec7641b2f2a7e9714b8f641bdaf0fb20a8
_Kielson <127547772+Kielson123@users.noreply.github.com>
2025-03-17 18:02:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function onPaiClicked(){
+    document.getElementById(`body`).innerHTML += `<p>PAI</p>`
+}
+
+function onInfClicked(){
+    document.getElementById(`body`).innerHTML += `<p>Informatyka</p>`
+}
