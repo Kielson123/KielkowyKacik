bb3aeaa0f3cdf5fa869675f00bcb3c4eb318f4e2
Gracek12 <gracjanprzymus12@gmail.com>
2025-03-19 23:40:20 +0100

    Pierwszy kodzik i problemy psychiczne

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 let tekst = ""
-let selected_section = "inf"
 
-function onPaiClicked(){
-    selected_section = "pai"
-    tekst = `<p>PAI</p>`
+function onInfClicked(){
+    tekst = `
+        <section class="python">
+            <h1>Sortowanie bąbelkowe</h1>
+            <article>
+                <p class="code">
+                    <span id="kwd">import</span> random<br><br>
+                    n = <span id="fun">int</span>(<span id="fun">input</span>(<span id="str">"Podaj wielkość zbioru: "</span>))<br>
+                    wart_min = <span id="fun">int</span>(<span id="fun">input</span>(<span id="str">"Podaj najmniejszą możliwą liczbę: "</span>))<br>
+                    wart_max = <span id="fun">int</span>(<span id="fun">input</span>(<span id="str">"Podaj największą możliwą liczbę: "</span>))<br>
+                    A = []<br>
+                    <span id="kwd">for</span> i <span id="kwd">in</span> <span id="fun">range</span>(n):<br>&emsp;&emsp;
+                        A.append(random.<span id="fun">randint</span>(wart_min, wart_max))<br>
+                    <span id="fun">print</span>(A)<br>
+                    <span id="kwd">for</span> i <span id="kwd">in</span> <span id="fun">range</span>(1, n):<br>&emsp;&emsp;
+                    <span id="kwd">for</span> j <span id="kwd">in</span> <span id="fun">range</span>(0, n - i):<br>&emsp;&emsp;&emsp;&emsp;
+                    <span id="kwd">if</span> A[j] > A[j + 1]:<br>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;
+                                A[j], A[j + 1] = A[j + 1], A[j]<br>
+                    <span id="fun">print</span>(A)
+                </p>
+            </article>
+        </section>
+    `
     addHtmlToMain()
 }
 
-function onInfClicked(){
-    selected_section = "inf"
-    tekst = `<p>Informatyka</p>`
+function onPaiClicked(){
+    tekst = `
+        b
+    `
     addHtmlToMain()
 }
 
 function addHtmlToMain(){
     document.getElementById('main_block').innerHTML = tekst 
 }
+
+onInfClicked()
 
 
 
