0105cb13aae5b327212391ae72196793c65a158d
_Kielson <gracjanprzymus12@gmail.com>
2025-03-19 16:49:44 +0100

    Design update

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+let tekst = ""
+let selected_section = "inf"
+
 function onPaiClicked(){
-    document.getElementById(`body`).innerHTML += `<p>PAI</p>`
+    selected_section = "pai"
+    tekst = `<p>PAI</p>`
+    addHtmlToMain()
 }
 
 function onInfClicked(){
-    document.getElementById(`body`).innerHTML += `<p>Informatyka</p>`
+    selected_section = "inf"
+    tekst = `<p>Informatyka</p>`
+    addHtmlToMain()
 }
+
+function addHtmlToMain(){
+    document.getElementById('main_block').innerHTML = tekst 
+}
+
+
+
