ac68672d4d9032bc02c7fdb076c3df0476047639
Kielson <gracjanprzymus12@gmail.com>
2025-09-04 22:19:31 +0200

    Updated the site functionality

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 function clamp(number, min, max) {
     return Math.max(min, Math.min(number, max));
 }
+
+function addHtml() {
+    let tags = document.getElementsByTagName("*")
+    for (let i = 0; i < tags.length; i++) {
+        let elmnt = tags[i] 
+        let file = elmnt.getAttribute("include-html")
+        if (file) {
+            let xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Page not found.";
+                    }
+                    
+                    elmnt.removeAttribute("include-html");
+                    addHtml();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+
+            return;
+        }
+    }
+}
+
+function openTab(event, tabName) {
+    let tabcontent = document.getElementsByClassName("tab-content");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+  
+    let tablinks = document.getElementsByClassName("tab-links");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(tabName).style.display = "flex";
+    event.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
